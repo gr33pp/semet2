5fecf3e7dc061aa328f315149d7f86a008974e90
Dimeji <ukwedjedimeji@gmail.com>
2024-07-14 22:17:43 +0100

    cm

--- build_files.sh
+++ build_files.sh
@@ -1,15 +1,4 @@
-# build_files.sh
-# Install pip if it's not available
-command -v pip > /dev/null 2>&1 || { 
-    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-    python get-pip.py
-}
-
-# Install dependencies
-pip install -r requirements.txt
-
-# Collect static files
-python manage.py collectstatic --noinput
-
-# Run database migrations
-python manage.py migrate
+echo "BUILD START"
+python3 -m pip install -r requirements.txt
+python3 manage.py collectstatic --noinput --clear
+echo "BUILD END"
