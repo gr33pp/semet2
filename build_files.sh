2e8236e616d7721a6b520c0eed3b2a3ca24fdf09
Ramon Bello <133546822+gr33pp@users.noreply.github.com>
2024-07-15 10:13:11 +0100

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,14 @@
+#!/usr/bin/env bash
 echo "BUILD START"
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput --clear
+# Exit on error
+set -o errexit
+
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install -r requirements.txt
+
+# Convert static asset files
+python manage.py collectstatic --no-input
+
+# Apply any outstanding database migrations
+python manage.py migrate
 echo "BUILD END"
